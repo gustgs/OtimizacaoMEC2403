085a42f82052998768f97a1801eedf40bd5fb9d8
gustgs <gustavohgs@gmail.com>
2023-04-01 02:01:00 -0300

    melhoria do codigo do passo constante com definicao do sentido de busca

--- Lista1/linear_search_methods.py
+++ Lista1/linear_search_methods.py
@@ -10,27 +10,36 @@ def passo_cte(direcao, P1, f, step = 0.01, modulo_direcao = 1000):
     
     #calcula o vetor unitario na direção de busca solicitada
     direcao_unitaria = dirUnit(direcao)
+    
+    #epsilon
+    eps=0.00000001
+    
+    #define o sentido unitario correto de busca
+    if (f(P1 - eps*direcao_unitaria) > f(P1 + eps*direcao_unitaria)):
+        sentido_busca = direcao_unitaria
+    else:
+        sentido_busca = -direcao_unitaria
         
     #iteracao em alpha variando de 0 até o modulo informado da direção(opcional) ou até 1000 (default)
     for alpha in np.arange(0, modulo_direcao, step):
         # atualiza os valores de Pmin e alpha min com os valores de cada step
-        Pmin = P1 + alpha*direcao_unitaria
+        Pmin = P1 + alpha*sentido_busca
         alpha_min = alpha
                 
         #verifica se a funcao fica ascendente no proximo step
         #caso positivo, a busca unidimensional termina e os valores de Pmin e alpha min já estão guardados
-        if (f(Pmin) < f(P1 + (alpha+step)*direcao_unitaria)):
+        if (f(Pmin) < f(P1 + (alpha+step)*sentido_busca)):
             break
         
         #retorna o Pmin = [x1,x2]  e o intervalo de busca = [alpha min, alpha min + step]                 
-    return Pmin, np.array([alpha_min, alpha_min+step])
+    return Pmin, np.array([alpha_min, alpha_min+step]), sentido_busca
     
-def bissecao(intervalo, direcao, P1, f, tol=0.00001):
+def bissecao(intervalo, sentido_busca, P1, f, tol=0.00001):
     #linear search pelo método da bisseção
     #função estruturada de forma recursiva
     
-    #calcula o vetor unitario na direção de busca solicitada
-    direcao_unitaria = dirUnit(direcao)
+    #epsilon
+    eps = 0.00000001
     
     #atribui os limites superior e inferior da busca à variáveis internas do método
     alpha_upper = intervalo[1]
@@ -39,7 +48,7 @@ def bissecao(intervalo, direcao, P1, f, tol=0.00001):
     #atualiza o valor de alpha min (igual ao último alpha med) e  Pmin em cada chamada do método
     alpha_med = (alpha_lower + alpha_upper)/2
     alpha_min = alpha_med
-    Pmin = P1 + alpha_min*direcao_unitaria    
+    Pmin = P1 + alpha_min*sentido_busca    
     
     #condicao de convergencia
     if (alpha_upper - alpha_lower) <= tol:
@@ -48,8 +57,8 @@ def bissecao(intervalo, direcao, P1, f, tol=0.00001):
     else:
         #caso negativo, verifica se o lado à esquerda ou à direita do alpha med deve ser descartado
         # e chama, recursivamente, o metodo da bisseção com o intervalo restante
-        f1 = f(P1 + (alpha_med - tol)*direcao_unitaria)
-        f2 = f(P1 + (alpha_med + tol)*direcao_unitaria)
+        f1 = f(P1 + (alpha_med - eps)*sentido_busca)
+        f2 = f(P1 + (alpha_med + eps)*sentido_busca)
         
         #    verifica se o valor de f à esquerda é maior do que o valor de f à direita do alpha med
         if (f1 > f2):
@@ -58,19 +67,17 @@ def bissecao(intervalo, direcao, P1, f, tol=0.00001):
             intervalo[0] = alpha_lower
             
             #chama novamente o método com o novo intervalo de busca
-            return  bissecao(intervalo, direcao_unitaria, P1, f, tol)
+            return  bissecao(intervalo, sentido_busca, P1, f, tol)
         else:
             #caso negativo, define novo intervalo de busca como sendo do alpha lower atual até o alpha med atual
             alpha_upper = alpha_med
             intervalo[1] = alpha_upper 
             #chama novamente o método com o novo intervalo de busca           
-            return  bissecao(intervalo, direcao_unitaria, P1, f, tol)
+            return  bissecao(intervalo, sentido_busca, P1, f, tol)
 
-def secao_aurea(intervalo, direcao, P1, f, tol=0.00001):
+def secao_aurea(intervalo, sentido_busca, P1, f, tol=0.00001):
     #linear search pelo método da seção áurea
     
-    #calcula o vetor unitario da direção de busca solicitada
-    direcao_unitaria = dirUnit(direcao)
     #atribui os limites superior e inferior da busca à variáveis internas do método
     alpha_upper = intervalo[1]
     alpha_lower = intervalo[0]
@@ -84,8 +91,8 @@ def secao_aurea(intervalo, direcao, P1, f, tol=0.00001):
     alpha_d = alpha_lower + Ra*beta 
     
     #primeira iteração avalia f nos 2 pontos selecionados pela razao aurea
-    f1 = f(P1 + alpha_e*direcao_unitaria)
-    f2 = f(P1 + alpha_d*direcao_unitaria)
+    f1 = f(P1 + alpha_e*sentido_busca)
+    f2 = f(P1 + alpha_d*sentido_busca)
     
     #loop enquanto a convergência não for obtida
     while (beta > tol):
@@ -100,7 +107,7 @@ def secao_aurea(intervalo, direcao, P1, f, tol=0.00001):
             beta = alpha_upper - alpha_lower
             #alpha_e = alpha_lower + (1-Ra)*beta
             alpha_d = alpha_lower + Ra*beta 
-            f2 = f(P1 + alpha_d*direcao_unitaria)
+            f2 = f(P1 + alpha_d*sentido_busca)
         else:
             #caso negativo, define novo intervalo variando de alpha_lower até alpha_d
             # e aproveita os valores anteriores de alpha_e e f1 como novos alpha_d e f2
@@ -112,41 +119,11 @@ def secao_aurea(intervalo, direcao, P1, f, tol=0.00001):
             beta = alpha_upper - alpha_lower
             alpha_e = alpha_lower + (1-Ra)*beta
             #alpha_d = alpha_lower + Ra*beta 
-            f1 = f(P1 + alpha_e*direcao_unitaria)
+            f1 = f(P1 + alpha_e*sentido_busca)
             
     # calcula Pmin e alpha min após convergência
     alpha_med = (alpha_lower + alpha_upper)/2
     alpha_min = alpha_med
-    Pmin = P1 + alpha_min*direcao_unitaria
+    Pmin = P1 + alpha_min*sentido_busca
     
     return Pmin, alpha_min
-
-#metodo nao utilizado por nao ser otimizado nas chamadas de f
-def secao_aurea_recursiva(intervalo, direcao, P1, f, tol=0.00001):
-    #nao é a melhor implementacao pois chama funcao f 2vezes em cada iteração
-    
-    #calcula o vetor unitario da direção de busca solicitada
-    dir_unit = dirUnit(direcao)
-    
-    alpha_upper = intervalo[1]
-    alpha_lower = intervalo[0]
-    alpha_med = (alpha_lower + alpha_upper)/2
-    Pmin = P1 + alpha_med*dir_unit
-    
-    beta = alpha_upper - alpha_lower
-    Ra = (math.sqrt(5)-1)/2
-    
-    if beta <= tol :
-        return Pmin, alpha_med
-    else:
-        alpha_e = alpha_lower + (1-Ra)*beta
-        alpha_d = alpha_lower + Ra*beta
-        f1 = f(P1 + alpha_e*dir_unit)
-        f2 = f(P1 + alpha_d*dir_unit)
-        if (f1 > f2):
-            alpha_lower = alpha_e
-            intervalo[0] = alpha_lower            
-        else:
-            alpha_upper = alpha_d
-            intervalo[1] = alpha_upper
-    return secao_aurea(intervalo, dir_unit, P1, f, tol)
