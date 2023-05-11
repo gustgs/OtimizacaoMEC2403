50d0c2b6f31c83809e9c927b42643fb02338211f
gustgs <gustavohgs@gmail.com>
2023-05-10 22:22:32 -0300

    ajuste no codigo do passo constane. Check da derivada em cada passo e tb correcao do uso do eps que antes usava a direcao e agora usa a direcao unitaria

--- Trabalho1/line_search_methods.py
+++ Trabalho1/line_search_methods.py
@@ -1,37 +1,47 @@
 import numpy as np
-
-def passo_cte(direcao, P1, f, step = 0.01):
-    #linear search pelo metodo do passo constante
-    
-    #epsilon
-    eps=0.00000001
-   
-    #define o sentido unitario correto de busca
-    if (f(P1) >= f(P1 + eps*direcao)):
+ 
+def passo_cte(direcao, P0, f, eps = 1E-8, step = 0.01):
+    #line search pelo metodo do passo constante
+      
+    #define o sentido correto de busca
+    if (f(P0 - eps*(direcao/np.linalg.norm(direcao))) > f(P0 + eps*(direcao/np.linalg.norm(direcao)))):
         sentido_busca = direcao.copy()
         flag = 0
     else:
         sentido_busca = -direcao.copy()
         flag = 1
-    P = P1.copy()
+        
+    P = P0.copy()
     P_next = P + step*sentido_busca
     alpha = 0
+   
     while (f(P) > f(P_next)):           
         alpha = alpha + step
-        P = P1 + alpha*sentido_busca
-        P_next = P1 + (alpha+step)*sentido_busca
-        
-    Pmin = P.copy()
-    if(flag == 1):
-        alpha_min = -alpha
-    else:
-        alpha_min = alpha
-        
-    #retorna o Pmin = [x1,x2]  e o intervalo de busca = [alpha min, alpha min + step]                 
-    return Pmin, np.array([alpha, alpha + step]), sentido_busca, alpha_min
+        P = P0 + alpha*sentido_busca
+        P_next = P0 + (alpha+step)*sentido_busca
+        if (f(P - eps*(sentido_busca/np.linalg.norm(sentido_busca))) < f(P)):
+            alpha = alpha - step
+            break
     
-def secao_aurea(intervalo, direcao, sentido_busca, P1, f, tol=0.00001):
-    #linear search pelo metodo da secao aurea
+    intervalo = np.array([alpha, alpha + step])
+    
+    if(flag == 1):
+        intervalo = -intervalo
+        
+    #retorna o intervalo de busca = [alpha min, alpha min + step]                 
+    return intervalo
+    
+def secao_aurea(intervalo, direcao, P0, f, tol=0.00000001):
+    #line search pelo metodo da secao aurea
+    
+    #verifica o sentido da busca
+    if(intervalo[1] < 0):
+        intervalo = -intervalo
+        sentido_busca = -direcao.copy()
+        flag = 1
+    else:
+        sentido_busca = direcao.copy()
+        flag = 0
     
     #atribui os limites superior e inferior da busca a variaveis internas do metodo
     alpha_upper = intervalo[1]
@@ -46,8 +56,8 @@ def secao_aurea(intervalo, direcao, sentido_busca, P1, f, tol=0.00001):
     alpha_d = alpha_lower + Ra*beta 
     
     #primeira iteracao avalia f nos 2 pontos selecionados pela razao aurea
-    f1 = f(P1 + alpha_e*sentido_busca)
-    f2 = f(P1 + alpha_d*sentido_busca)
+    f1 = f(P0 + alpha_e*sentido_busca)
+    f2 = f(P0 + alpha_d*sentido_busca)
     
     #loop enquanto a convergência nao for obtida
     while (beta > tol):
@@ -62,7 +72,7 @@ def secao_aurea(intervalo, direcao, sentido_busca, P1, f, tol=0.00001):
             beta = alpha_upper - alpha_lower
             #alpha_e = alpha_lower + (1-Ra)*beta
             alpha_d = alpha_lower + Ra*beta 
-            f2 = f(P1 + alpha_d*sentido_busca)
+            f2 = f(P0 + alpha_d*sentido_busca)
         else:
             #caso negativo, define novo intervalo variando de alpha_lower ate alpha_d
             # e aproveita os valores anteriores de alpha_e e f1 como novos alpha_d e f2
@@ -74,14 +84,13 @@ def secao_aurea(intervalo, direcao, sentido_busca, P1, f, tol=0.00001):
             beta = alpha_upper - alpha_lower
             alpha_e = alpha_lower + (1-Ra)*beta
             #alpha_d = alpha_lower + Ra*beta 
-            f1 = f(P1 + alpha_e*sentido_busca)
+            f1 = f(P0 + alpha_e*sentido_busca)
             
     # calcula Pmin e alpha min apos convergência
     alpha_med = (alpha_lower + alpha_upper)/2
     alpha_min = alpha_med
-    Pmin = P1 + alpha_min*sentido_busca
     
-    if (not(direcao == sentido_busca).all()):
+    if (flag == 1):
         alpha_min = -alpha_min
     
-    return Pmin, alpha_min
+    return alpha_min
--- Trabalho1/osr_methods.py
+++ Trabalho1/osr_methods.py
@@ -1,227 +1,66 @@
 import numpy as np
-import line_search_methods as lsm
-from timeit import default_timer as timer
 
-def univariante(P0, f, grad_f, maxiter, tol, line_step):
-    start = timer()
-    dimens = P0.size
-    Pmin = P0.copy()
-    passos = 0
-    ciclos = 0   
-    dirs = np.zeros(shape=(dimens,dimens))
-    for i in np.arange(dimens):
-        ek = np.zeros(dimens)
-        ek[i] = 1
-        dirs[i] = ek.copy()
-      
-    listPmin = []
-    listPmin.append(Pmin)   
-    while(np.linalg.norm(grad_f(Pmin)) >tol):
-        ciclos = ciclos + 1
-        for i in np.arange(dimens):
-            dir = dirs[i]
-            P_pss_cte, intervalo, sentido, alpha_pc = lsm.passo_cte(dir, Pmin, f, step=line_step)
-            Pmin, alpha_sau = lsm.secao_aurea(intervalo, dir, sentido, Pmin, f)
-            passos = passos + 1
-            listPmin.append(Pmin)
-            if(np.linalg.norm(grad_f(Pmin)) <= tol):
-                break
-            if(passos == maxiter):
-                print('Nao convergiu !')
-                end = timer()
-                return listPmin, passos, end-start
-    end = timer()
-    return listPmin, ciclos , passos, end-start
-
-def powellv2(P0, f, grad_f, maxiter, tol, line_step):
+def univariante(passo, dimens):
+    indice = passo%dimens - 1
+    if (indice == -1) :
+        indice = dimens - 1
+    ek = np.zeros(dimens)
+    ek[indice] = 1
     
-    grad = grad_f(P0)
-    norm_grad = np.linalg.norm(grad)
-    Min = P0.copy()
+    return ek
     
-    e1 = np.array([1,0])
-    e2 = np.array([0,1])
-    canonicas = np.array([e1, e2], dtype=float)
-    passos = 0
-    ciclos = 0
-    direcoes = canonicas.copy()
-    while(norm_grad > tol):
+def powell(P, P1, direcoes, passos, ciclos, dimens):
+    indice = passos%(dimens + 1) - 1
+    if (indice == -1):
+        dir = P - P1
+        direcoes[dimens - 1] = dir        
+    elif (indice == 0):
         ciclos = ciclos + 1
-        P1 = Min
-        if (ciclos%4 == 0):
-            direcoes = canonicas.copy()
-        
-        for i in np.arange(3):
-            if (i == 2):
-                d = Min - P1
-                direcoes[1] = d                
-            else:
-                d = direcoes[i].copy()
-              
-            P_pss_cte, intervalo, sentido, alpha = lsm.passo_cte(d, Min, f, step=line_step)
-            Min = lsm.secao_aurea(intervalo, sentido, Min, f)[0]
-            grad = grad_f(Min)
-            norm_grad = np.linalg.norm(grad)
-            passos = passos + 1
-            
-            if (i==1):
-                direcoes[0] = direcoes[1].copy()
-            if (passos == maxiter):
-                print('Nao convergiu')
-                break
-            print(f'Ciclo={ciclos}, Passo={passos}, d={d}, P={Min}, Grad={grad}, Norm_grad={norm_grad}')
-        
-        if(passos == maxiter):
-            break
-    return Min, ciclos, passos                  
-
-def powell(P0, f, grad_f, maxiter, tol, line_step):
-    start = timer()
-    dimens = P0.size
-    Pmin = P0.copy()
-    #grad = nd.Gradient(f)
-    passos = 0
-    ciclos = 0
-    dirs_canonicas = np.zeros(shape=(dimens+1,dimens))
-    for j in np.arange(dimens):
-        ek = np.zeros(dimens)
-        ek[j] = 1
-        dirs_canonicas[j] = ek
-    
-    dirs = dirs_canonicas.copy()    
-    listPmin = []
-    listPmin.append(Pmin)    
-    while(np.linalg.norm(grad_f(Pmin)) > tol):
-        P1 = Pmin
-        ciclos = ciclos + 1
-        
         if (ciclos%(dimens+2) == 0):
-            dirs = dirs_canonicas.copy()
-            
-        for i in np.arange(dimens+1):
-            if(i == dimens):
-                dirs[i] = Pmin - P1
+            direcoes = np.eye(dimens, dtype=float)
+        P1 = P.copy()
+        dir = direcoes[indice].copy()
+    else:
+        dir = direcoes[indice].copy()
+        direcoes[indice-1] = dir
+  
+    return dir, direcoes, P1, ciclos            
+
+def newtonRaphson(P, grad_P, hessian_f):
+    return -np.linalg.inv(hessian_f(P)).dot(grad_P)
+
+def steepestDescent(grad):
+    return -grad
+
+def fletcherReeves(dir_last, grad, grad_last, passo):
+    if passo == 1:
+        grad_last = grad.copy()
+        return -grad, grad_last
+    else:
+        beta = (np.linalg.norm(grad)/np.linalg.norm(grad_last))**2
+        grad_last = grad.copy()
+        return -grad + beta*dir_last, grad_last
+    
+def bfgs(P, P_last, grad, grad_last, S_last, passo, dimens):
+    if (passo == 1):
+        dir = -S_last.dot(grad)
+    else:
+        delta_x_k = P - P_last
+        delta_g_k = grad - grad_last
+        
+        #para o numpy, vetor 1-D linha e vetor coluna sao a mesma coisa (nao e necessrio transpor)
+        #matrizes
+        A = np.outer(delta_x_k, np.transpose(delta_x_k))
+        B = S_last.dot(np.outer(delta_g_k, np.transpose(delta_x_k)))
+        C = np.outer(delta_x_k, np.transpose(S_last.dot(delta_g_k)))
+        
+        #Escalares        
+        d = np.transpose(delta_x_k).dot(delta_g_k)
+        e = np.transpose(delta_g_k).dot(S_last.dot(delta_g_k))
                 
-            dir = dirs[i].copy()
-            P_pss_cte, intervalo, sentido, alpha = lsm.passo_cte(dir, Pmin, f, step=line_step)
-            Pmin = lsm.secao_aurea(intervalo, dir, sentido, Pmin, f)[0]
-            listPmin.append(Pmin) 
-            passos = passos + 1
-            print(f'Ciclo={ciclos}, Passso {passos}: dir={dir}, grad={grad_f(Pmin)}, norm_grad={np.linalg.norm(grad_f(Pmin))} P={Pmin} ')
-        
-            if( i > 0):
-                dirs[i-1] = dirs[i].copy()
-        
-            if(np.linalg.norm(grad_f(Pmin)) < tol):
-                break
-            if(passos == maxiter):
-                print('Nao convergiu !')
-                end = timer()
-                return listPmin, passos, end-start
-    end = timer()
-    return listPmin, ciclos, passos, end-start
-
-def newtonRaphson(P0, f, grad_f, hessian_f, maxiter, tol, line_step):
-    start = timer()
-    dimens = P0.size
-    Pmin = P0.copy()
-    passos = 0
-    
-    listPmin = []
-    listPmin.append(Pmin)
-    grad = grad_f(Pmin)
-    norm_grad = np.linalg.norm(grad)
-    while(norm_grad > tol):
-        if(passos == maxiter):
-            print('Nao convergiu !')
-            end = timer()
-            return listPmin, passos, end-start
-        hess = hessian_f(Pmin)
-        dir = -np.linalg.inv(hess).dot(grad)
-        P_pss_cte, intervalo, sentido, alpha = lsm.passo_cte(dir.copy(), Pmin.copy(), f, step=line_step)
-        Pmin, alpha_min = lsm.secao_aurea(intervalo, dir.copy(), sentido, Pmin.copy(), f)
-        listPmin.append(Pmin.copy()) 
-        grad = grad_f(Pmin)
-        norm_grad = np.linalg.norm(grad)
-        
-        passos = passos + 1
-        #print(f'Passo{passos}, P={Pmin}, alpha={alpha_min}, d={dir}, norm_grad={norm_grad}, grad={grad}, hessian={hess}')
-    end = timer()
-    return listPmin, passos, end-start
-
-def steepestDescent(P0, f, grad_f, maxiter, tol, line_step):
-    start = timer()
-    dimens = P0.size
-    Pmin = P0.copy()
-    grad = grad_f(Pmin)
-    norm_grad = np.linalg.norm(grad)
-    passos = 0
-    
-    listPmin = []
-    listPmin.append(Pmin)     
-    while(norm_grad > tol):
-        if(passos == maxiter):
-            print('Nao convergiu !')
-            end = timer()
-            return listPmin, passos, end-start
-        dir = -grad
-        P_pss_cte, intervalo, sentido, alpha = lsm.passo_cte(dir, Pmin, f, line_step)
-        Pmin, alpha_min = lsm.secao_aurea(intervalo, dir, sentido, Pmin, f)
-        listPmin.append(Pmin) 
-        passos = passos + 1
-        grad = grad_f(Pmin)
-        norm_grad = np.linalg.norm(grad)
-        print(f'Passo{passos}, P={Pmin}, alpha={alpha_min}, d={dir}, norm_grad={norm_grad}, grad={grad}')
-    end = timer()             
-    return listPmin, passos, end-start
-
-def fletcherReeves(P0, f, grad_f, maxiter, tol, line_step):
-    start = timer()
-    dimens = P0.size
-    Pmin = P0.copy()
-    grad = grad_f(Pmin)
-    passos = 0
-    
-    listPmin = []
-    listPmin.append(Pmin)
-    
-    #primeiro passo
-    grad_k = grad_f(P0)
-    norm_grad = np.linalg.norm(grad_k)
-    
-    dir = -grad_k
-    dir_k = dir.copy()
-    
-    if(norm_grad < tol):
-        end = timer()
-        return listPmin, passos, end-start
-    
-    P_pss_cte, intervalo, sentido, alpha = lsm.passo_cte(dir, Pmin, f)
-    Pmin = lsm.secao_aurea(intervalo, dir, sentido, Pmin, f)[0]
-    listPmin.append(Pmin) 
-    passos = passos + 1
-    grad_knext = grad_f(Pmin)
-    norm_grad = np.linalg.norm(grad_knext)
-    print(f'Passso {passos}: dir={dir}, grad_k={grad_k}, grad_k+1={grad_knext}, P={Pmin} ')
-         
-    while(norm_grad > tol):
-        if(passos == maxiter):
-            print('Nao convergiu !')
-            end = timer()
-            return listPmin, passos, end-start
-        #beta = (np.transpose(grad_knext)).dot(grad_knext)/((np.transpose(grad_k)).dot(grad_k))
-        beta = (np.linalg.norm(grad_knext)/np.linalg.norm(grad_k))**2
-        dir = -grad_knext + beta*dir_k
-        P_pss_cte, intervalo, sentido, alpha = lsm.passo_cte(dir/(np.linalg.norm(dir)), Pmin, f, step=line_step)
-        Pmin = lsm.secao_aurea(intervalo, dir/(np.linalg.norm(dir)), sentido, Pmin, f)[0]
-        listPmin.append(Pmin) 
-        
-        grad_k = grad_knext.copy()
-        dir_k = dir.copy()
-        grad_knext = grad_f(Pmin)
-        norm_grad = np.linalg.norm(grad_knext)
-        passos = passos + 1
-        print(f'Passso {passos}: dir={dir}, grad_k={grad_k}, grad_k+1={grad_knext}, P={Pmin}')
-    end = timer()
-    
-    return listPmin, passos, end-start
+        S = S_last + (d + e)*A/(d**2) - (B + C)/d
+        dir = -S.dot(grad)
+        S_last = S.copy()
+    P_last = P
+    grad_last = grad
+    return dir, P_last, grad_last, S_last
